a3355095b7376c3d73df0d24befeda8c5c7fddca
Hilmerson <hilmer.viktor@gmail.com>
2026-07-26 14:28:07 +0200

    Use test recipient for contact form mails

--- public/api/contact.php
+++ public/api/contact.php
@@ -26,7 +26,9 @@ if ($meno === '' || $sprava === '' || !filter_var($email, FILTER_VALIDATE_EMAIL)
     exit('Chýbajúce alebo neplatné údaje. Vráťte sa späť a skúste to znova.');
 }
 
-$to      = 'office@i-studio.sk';
+// TESTOVANIE: pred spustením naostro prepnúť na office@i-studio.sk
+$to      = 'vktrhilmer21@gmail.com';
+// $to   = 'office@i-studio.sk';
 $subject = '=?UTF-8?B?' . base64_encode('Dopyt z webu i-studio.sk' . ($kategoria !== '' ? " – $kategoria" : '')) . '?=';
 $body    = "Meno: $meno\nE-mail: $email\nTelefón: $telefon\nZáujem: $kategoria\n\nSpráva:\n$sprava\n";
 $headers = [
